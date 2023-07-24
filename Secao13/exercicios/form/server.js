b0cd5a1a586a562095eeef7524f39aaa3e199cc3
Henrique-Assme <heniassme@gmail.com>
2023-07-24 20:57:12 -0300

    Add: secao 13

---
+++ Secao13/exercicios/form/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (request, response, _next) => {
+    console.log(request.body)
+    response.send('<h1>Parabéns. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (request, response, _next) => {
+    console.log(request.params.id)
+    console.log(request.body)
+    response.send('<h1>Parabéns. Usuário alterado!</h1>')
+})
+
+app.listen(3003)
