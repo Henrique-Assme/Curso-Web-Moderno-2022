43ebfb4de4e0956e042956e617902d8925c25aae
Henrique-Assme <heniassme@gmail.com>
2023-08-25 16:32:57 -0300

    Add: first components

---
+++ Secao24-Projeto Cadastro de Usuário/crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+import React from "react";
+
+const Footer = props => (
+    <footer className="footer">
+        Footer
+    </footer>
+)
+
+export default Footer
---
+++ Secao24-Projeto Cadastro de Usuário/crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,10 @@
+import './Nav.css';
+import React from "react";
+
+const Nav = props => (
+    <aside className="menu-area">
+        Navegação
+    </aside>
+)
+
+export default Nav
