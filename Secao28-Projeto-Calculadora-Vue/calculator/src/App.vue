dfac5a76c50a7de08dad4a7441efefb1f6fafabd
Henrique-Assme <heniassme@gmail.com>
2023-09-04 23:06:16 -0300

    Add secao28

---
+++ Secao28-Projeto-Calculadora-Vue/calculator/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <CalculatorVue />
+  </div>
+</template>
+
+<script>
+import CalculatorVue from './main/CalculatorVue'
+
+export default {
+  name: 'App',
+  components: { CalculatorVue }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  display: flex;
+  margin: 0; 
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+
+  color: #fff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
